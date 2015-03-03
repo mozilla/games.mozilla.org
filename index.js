68b5cae31848d15567015071f11f8f8e3779847c
Christopher Van <cvan@mozilla.com>
2015-03-03 08:36:12 -0800

    🎉🎊🎮 GDC LAUNCH

--- index.js
+++ index.js
@@ -23,46 +23,17 @@ internals.fileServer = new nodeStatic.Server('./public', internals.nodeStaticOpt
 
 
 var server = http.createServer(function (req, res) {
-  var credentials = basicAuth(req);
-  var authChunks = (process.env.AUTH_GDC || ':').split(':');
-  var authValid = (credentials &&
-                   credentials.name === authChunks[0] &&
-                   credentials.pass === authChunks[1]);
-
-  // Protect only the `/gdc/` directory.
-  function ipInRange(ip) {
-    var blocks = (ip || '').split('.').map(function (num) {
-      return parseInt(num, 10);
-    });
-    return (blocks[0] === 63 &&
-            blocks[1] === 245 &&
-            blocks[2] >= 208 && blocks[2] <= 223 &&
-            blocks[3] >=0 && blocks[3] <= 255);
-  }
-
-  if (req.url === '/ip') {
-    res.writeHead(200, {'content-type': 'application/json'});
-    return res.end(JSON.stringify({
-      'req.connection.remoteAddress': req.connection.remoteAddress,
-      'req.connection.socket.remoteAddress': req.connection.socket && req.connection.socket.remoteAddress,
-      'req.socket.remoteAddress': req.socket && req.socket.remoteAddress,
-      'x-forwarded-for': req.headers['x-forwarded-for']
-    }));
-  }
-
-  if (!ipInRange(req.headers['x-forwarded-for']) &&
-      process.env.AUTH_GDC && req.url.indexOf('/gdc/') === 0 && !authValid) {
-
-    return internals.fileServer.serveFile('/401.html', 401,
-      {'WWW-Authenticate': 'Basic realm="mozgames"'}, req, res);
-  }
-
   var urlBase = req.url.split('?')[0] || '';
   var urlExtension = urlBase.substr(urlBase.lastIndexOf('.'));
   if (['.woff', '.ttf', '.eot'].indexOf(urlExtension) !== -1) {
     var now = new Date();
     now.setSeconds(now.getSeconds() + internals.cacheExpiryDates.fonts);
     res.setHeader('Expires', now.toUTCString());
+  }
+
+  if (req.url.split('?')[0] === '/') {
+    res.writeHead(302, {'Location': '/gdc' + req.url});
+    return res.end();
   }
 
   req.addListener('end', function () {
